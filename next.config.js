8ac98227aa07337bcbc5a32a6f7a6e57e17292ae
Gjorgjioska <marina.gorgioska@gmail.com>
2022-03-19 15:19:19 +0100

    Working on my new project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ["https://bayut-production.s3.eu-central-1.amazonaws.com"],
+//   },
+// };
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ["bayut-production.s3.eu-central-1.amazonaws.com"],
+  },
+};
